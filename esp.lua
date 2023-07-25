a0d7d069e1de5be231f3116386dfcc8c4d55f7c9
nereidrbx <136233472+nereidrbx@users.noreply.github.com>
2023-07-25 23:53:54 +0300

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -337,26 +337,18 @@ end
 
 local function CharAdded(char)
     local p = plrs:GetPlayerFromCharacter(char)
-    if not char:FindFirstChild("HumanoidRootPart") then
-        local ev
-        ev = char.ChildAdded:Connect(function(c)
-            if c.Name == "HumanoidRootPart" then
-                ev:Disconnect()
-                ESP:Add(char, {
-                    Name = p.Name,
-                    Player = p,
-                    PrimaryPart = c
-                })
-            end
-        end)
-    else
+    local lowerTorso = char:FindFirstChild("LowerTorso")
+    local humanoidRootPart = char:FindFirstChild("HumanoidRootPart")
+
+    if lowerTorso and humanoidRootPart then
         ESP:Add(char, {
             Name = p.Name,
             Player = p,
-            PrimaryPart = char.HumanoidRootPart
+            PrimaryPart = humanoidRootPart
         })
     end
 end
+
 local function PlayerAdded(p)
     p.CharacterAdded:Connect(CharAdded)
     if p.Character then
